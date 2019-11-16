2e02206103062b7f31e22d2437f6c617d32055b8
JuliaOlshevska <juliafaer2@gmail.com>
2019-11-16 22:32:06 +0200

    Light Bulb

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,22 @@
-alert("My new page");
+function onPageLoad() {
+  console.log("Page has loaded");
+}
+const lightBulbIsOn = document.getElementById("bulbIsOn");
+const lightBulbIsOff = document.getElementById("bulbIsOff");
+const backgroundColorOfBody = document.getElementsByTagName("body");
+const changeContentOfHeader = document.getElementById("changingOfContent");
+
+function lightIsOn() {
+  lightBulbIsOn.style.display = "inherit";
+  lightBulbIsOff.style.display = "none";
+  backgroundColorOfBody[0].style.backgroundColor = "rgb(250, 250, 11)";
+  changeContentOfHeader.innerText = "Bulb is: ON";
+  changeContentOfHeader.style.color = "black";
+}
+function lightIsOff() {
+  lightBulbIsOn.style.display = "none";
+  lightBulbIsOff.style.display = "inherit";
+  backgroundColorOfBody[0].style.backgroundColor = "rgb(56, 56, 56)";
+  changeContentOfHeader.innerText = "Bulb is: OFF";
+  changeContentOfHeader.style.color = "white";
+}
